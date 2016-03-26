eda18eef93d1a4f4293d49d4e96446d270044da0
Libo Liu <llb0536@aliyun.com>
2016-03-26 22:22:20 +0800

    add socks5 proxy

--- vgg_face_dataset/download_vgg_dataset.py
+++ vgg_face_dataset/download_vgg_dataset.py
@@ -1,10 +1,19 @@
 import os
+
+###### 必须加在urllib2之前
+###### 还必须pip install PySocks
+###### 这是全部的都用代理，应该把这部分加到download_image里的，第一次下载不用proxy，第二次之后用proxy
+import socket
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 65531)
+socket.socket = socks.socksocket
+###### 
 import urllib2
 import httplib
 
 import multiprocessing
 
-def download_image(url, save_name):
+
+def download_image(url, save_name,proxy = None):
 	try:
 		fp = urllib2.urlopen(url, timeout=20)
 		data = fp.read()
@@ -14,6 +23,11 @@ def download_image(url, save_name):
 		fid = open(save_name, 'w+b')
 		fid.write(data)
 		flag = True 
+		######### 此处应该判断文件的大小，如果有md5也需要校验md5
+		flsize = os.path.getsize(save_name)
+		if flsize < 10:
+			flag = False
+		######
 	except Exception:
 		print url + ' downloading io error...'
 		flag = False
@@ -41,6 +55,14 @@ def readList((list_path, save_path, log_path)):
 			continue
 
 		flag = download_image(url, filename)
+		#########  这里应该判断flag，如果有问题，那么重新下载，不然才记录
+		retrytimes = 0
+		while not flag:
+			flag = download_image(url, filename)
+			retrytimes = retrytimes + 1
+			if retrytimes > 5:
+				break
+			
 		if not flag:
 			fid_log.write(line)
 			#raw_input();
